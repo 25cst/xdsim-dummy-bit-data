4decc17be6778b12e810530e0f3fdeafd6bd73ad
Siriusmart <sirius@siri.ws>
2025-12-13 23:25:50 +0000

    use newer libxdsim

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,40 @@
-use libxdsim::v0::component::Type;
+use libxdsim::{
+    defs::PackageDefinition,
+    package_ident_v0,
+    v0::{ComponentType, PackageDefinitionV0, component::Data},
+};
 
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct Bit(pub bool);
 
-impl Type for Bit {
+impl Data for Bit {
     fn serialize(&self) -> Box<[u8]> {
         Box::new([if self.0 { 1 } else { 0 }])
     }
 
-    fn deserialize(bytes: Box<[u8]>) -> Self where Self: Sized {
+    fn deserialize(bytes: Box<[u8]>) -> Self
+    where
+        Self: Sized,
+    {
         assert_eq!(bytes.len(), 1);
         Bit(bytes[0] != 0)
     }
 }
 
 #[unsafe(no_mangle)]
-pub fn deserialize_boxed(bytes: Box<[u8]>) -> Box<dyn Type> {
+pub fn deserialize_boxed(bytes: Box<[u8]>) -> Box<dyn Data> {
     Box::new(Bit::deserialize(bytes))
+}
+
+#[unsafe(no_mangle)]
+pub fn package_definition() -> Box<dyn PackageDefinition> {
+    Box::new(PackageDefinitionV0 {
+        ident: package_ident_v0!("dummy-bit"),
+        component_type: ComponentType::Data,
+
+        authors: &["siriusmart"],
+        description: "An example bit data type.",
+        homepage: "https://github.com/25cst/xdsim-dummy-bit-data",
+    })
 }
